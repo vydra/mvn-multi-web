979a679413563b228af57059c31e54f00a773150
David Vydra <david.vydra@stormpath.com>
2014-02-17 22:45:44 -0800

    testing exploratory tesing

--- testing/explore.py
+++ testing/explore.py
@@ -3,7 +3,7 @@
 import string
 import random
 
-allchars = list(string.ascii_letters + string.punctuation)
+allchars = list(string.ascii_letters + '$#@!')
 for each in allchars:
   value = each + random.choice(allchars)
   print """- mvn -Dskip.surefire.tests=true -Dit.test=MailMergeExplore -Dvalue=%s integration-test:\n    path: simple-parent/simple-command""" % value
